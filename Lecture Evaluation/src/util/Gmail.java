4f457f38e76ec6f5557cc07fc534b2ebf5111d3b
jaeeunhyun <rosa2070@naver.com>
2021-03-10 17:42:06 +0900

    dddd

---
+++ Lecture Evaluation/src/util/Gmail.java
@@ -0,0 +1,13 @@
+package util;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+
+public class Gmail extends Authenticator {
+	
+	@Override
+	protected PasswordAuthentication getPasswordAuthentication() {
+		return new PasswordAuthentication("rosa207061@gmail.com", "gloria@@11");
+	}
+
+}
